d7a8020beed3f18dd61327ccc98997970d6d90c4
zejun1219 <118799534+zejun1219@users.noreply.github.com>
2023-06-27 22:01:02 +0800

    Update and rename companyRegister.html to companyRegister.php

--- companyRegister.php
+++ companyRegister.php
@@ -1,9 +1,49 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Company Registration</title>
+
     <link rel="stylesheet" href="company.css">
 </head>
+
 <body>
+<?php
+$servername = "localhost";  
+$username = "ma zejun";  
+$password = "mazejun2001"; 
+$dbname = "mahila";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("connect error: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $companyName = $_POST["name"];
+    $registrationNo = $_POST["registrationNo"];
+    $employeeCount = $_POST["employeeCount"];
+    $foundingYear = $_POST["foundingYear"];
+
+
+    $checkQuery = "SELECT * FROM company WHERE name = '$companyName'";
+    $checkResult = $conn->query($checkQuery);
+    if ($checkResult->num_rows > 0) {
+    
+        echo "company is exist";
+    } else {
+
+        $insertQuery = "INSERT INTO company (name, registration_no, employee_count, founding_year)
+                        VALUES ('$companyName', '$registrationNo', '$employeeCount', '$foundingYear')";
+
+        if ($conn->query($insertQuery) === TRUE) {
+            echo "register successful";
+        } else {
+            echo "register fail" . $conn->error;
+        }
+    }
+}
+
+$conn->close();
+?>
 <div class="registration-form">
         <h2 class="title">Company Registration</h2>
         <form method="POST" action="registration.php">

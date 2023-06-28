14a1f592beddad65a0833f9bd42a6f204bcfe8e5
zejun1219 <118799534+zejun1219@users.noreply.github.com>
2023-06-28 11:50:22 +0800

    Update companyRegister.php

--- companyRegister.php
+++ companyRegister.php
@@ -15,39 +15,40 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $foundingYear = $_POST["foundingYear"];
 
 
-    $checkQuery = "SELECT * FROM company WHERE name = '$companyName'";
+    $checkQuery = "SELECT * FROM company WHERE cname = '$companyName'";
     $checkResult = $conn->query($checkQuery);
     if ($checkResult->num_rows > 0) {
     
-        echo "company is exist";
+        echo "company already exists"; 
     } else {
 
-        $insertQuery = "INSERT INTO company (name, registration_no, employee_count, founding_year)
-                        VALUES ('$companyName', '$registrationNo', '$employeeCount', '$foundingYear')";
+        $insertQuery = "INSERT INTO company (cname, registration_number, number_of_employees, year_of_founding) 
+                        VALUES ('$companyName', '$registrationNo', '$employeeCount', '$foundingYear')"; 
 
         if ($conn->query($insertQuery) === TRUE) {
-            echo "register successful";
+            echo "registration successful"; 
         } else {
-            echo "register fail" . $conn->error;
+            echo "registration failed: " . $conn->error; 
         }
     }
 }
 
 $conn->close();
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Company Registration</title>
 
-    <link rel="stylesheet" href="company.css">
+    <link rel="stylesheet" href="CSS/company.css">
 </head>
 
 <body>
 
 <div class="registration-form">
         <h2 class="title">Company Registration</h2>
-        <form method="POST" action="registration.php">
+        <form method="POST" action="companyRegister.php">
             <div class="name">
                 <label for="name">Company name:</label><br>
                 <input type="text" name="name" id="name" class="text" required maxlength="25"><br><br>
@@ -59,6 +60,11 @@ $conn->close();
             <div class="retype-password">
                 <label for="retype-password">Password:</label>
                 <input type="password" name="retype-password" id="retype-password" class="text" required maxlength="50"><br><br>
+            </div>
+
+            <div class="registrationNo">
+                <label for="registrationNo">Registration No:</label>
+                <input type="number" name="registrationNo" id="registrationNo" class="text" required maxlength="1000"><br><br>
             </div>
 
             <div class="employeeCount">

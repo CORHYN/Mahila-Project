825923a8f1d67b32f77aa2518e6f7862ab89f715
zejun1219 <118799534+zejun1219@users.noreply.github.com>
2023-06-29 21:20:47 +0800

    mahila company

---
+++ GroupMahila/CompanyTest.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "mahila";
+
+$con = new mysqli($servername, $username, $password, $database);
+
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+if ($con->connect_error) {
+    die("Database connect fail : " . $con->connect_error);
+}
+
+$sql = "SELECT * FROM company WHERE email = '$email' AND pass = '$password'";
+$result = $con->query($sql);
+
+if ($result->num_rows == 1) {
+
+    header("Location: homepage.php");
+    exit();
+} else {
+   
+    echo "Email or password error !";   
+}
+
+$con->close();
+?>
---
+++ GroupMahila/delete.php
@@ -0,0 +1,23 @@
+<?php
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "mahila";
+
+//$conn = new mysqli($servername, $username, $password, $dbname);
+
+//if ($conn->connect_error) {
+//   die("connect database fail: " . $conn->connect_error);}
+
+//$id_to_delete = 0; 
+
+//$sql = "DELETE FROM company WHERE 
+//cname = $id_to_delete";
+
+//if ($conn->query($sql) === TRUE) {
+ //   echo "The job listing with ID $id_to_delete has been successfully deleted.";
+//} else {
+//    echo "Error deleting job listing: " . $conn->error;}
+
+//$conn->close();
+?>
